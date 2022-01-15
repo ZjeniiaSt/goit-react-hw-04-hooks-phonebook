fe8bbb137da38efcf337bba84fa902be4f16b6c5
ZjeniiaSt <zeniiastupjak@gmail.com>
2022-01-15 17:15:51 +0300

    reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import Container from './components/Container';
 import ContactForm from './components/ContactForm';
 import Filter from './components/Filter';
 import ContactList from './components/ContactList';
 import shortid from 'shortid';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_USER':
+      return { ...state, contacts: [...state.contacts, action.payload] };
+
+    case 'DELETE_USER': {
+      return { ...state, contacts: state.contacts.filter(user => user.id !== action.payload) };
+    }
+
+    default:
+      return state;
+  }
+};
+
+const init = {
+  contacts: JSON.parse(window.localStorage.getItem('contacts')) ?? [],
+};
+
 function App() {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
-  });
-
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
+  const [list, dispatch] = useReducer(reducer, init);
   const addContact = (name, number) => {
     if (
-      contacts.find(
+      list.contacts.find(
         contact => contact.name.toLowerCase() === name.toLowerCase() || contact.number === number,
       )
     ) {
@@ -27,26 +36,26 @@ function App() {
       return;
     }
 
-    const newContact = {
-      id: shortid.generate(),
-      name,
-      number,
-    };
-
-    setContacts(s => [newContact, ...s]);
+    dispatch({ type: 'ADD_USER', payload: { id: shortid(), name: name, number: number } });
   };
+
+  const deleteContact = id => {
+    dispatch({ type: 'DELETE_USER', payload: id });
+  };
+
+  const [filter, setFilter] = useState('');
   const changeFilter = ({ target }) => {
     setFilter(target.value);
   };
 
   const getvisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+    return list.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
   };
 
-  const deleteContact = id => {
-    setContacts(contacts.filter(contact => contact.id !== id));
-  };
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(list.contacts));
+  }, [list.contacts]);
 
   return (
     <>
